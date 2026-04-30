8ffafdc2b11593aa54aa47f6fe5bdd804e0a405b
CaseyWongWc <147561210+CaseyWongWc@users.noreply.github.com>
2026-04-30 10:34:44 +0000

    i sleep (i made square example from c17)

---
+++ data/Square.py
@@ -0,0 +1,14 @@
+class Square():
+              def __init__(self, side):
+                   self.side = side
+              @property
+              def side(self):
+                  return self.__side
+              @side.setter
+              def side(self, new_side):
+                  if new_side <= 0:
+                       raise ValueError("Side length must be positive")
+                  else:
+                       self.__side = new_side
+
+          
--- goog.py
+++ goog.py
@@ -1,26 +1,62 @@
+from sys import stdout
 from Helpers.helpings import *
 
 
+class Square:
+    def __init__(self, side):
+        self.side = side
+
+    @property
+    def side(self):
+        return self.__side
+
+    @side.setter
+    def side(self, new_side):
+        if new_side <= 0:
+            raise ValueError("Side length must be positive")
+        else:
+            self.__side = new_side
 
 
+test = Square(10)
+print(test.side)
+# out: 10
+test.side = 20
+print(test.side)
+# out: 20
 
+try:
+    test.side = -10
+except ValueError:
+    print("a")
+    # out: a
+print(test.side)
+# out: 20
+# raise NameError("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
 
+try:
+    raise ValueError("hello")
+except ValueError:
+    print("hello")
+    # out: hello
+else:
+    print("never")
 
 cmd("date")
-# out: Thu Apr 30 10:12:31 AM UTC 2026
+# out: Thu Apr 30 10:33:15 AM UTC 2026
 lsalf()
 # out: total 76
-# out: drwxr-xr-x 1 runner runner  434 Apr 30 10:09 ./
+# out: drwxr-xr-x 1 runner runner  434 Apr 30 10:33 ./
 # out: drwxrwxrwx 1 runner runner   58 Apr 30 05:50 ../
 # out: drwxr-xr-x 1 runner runner    0 Apr 16 10:23 .agents/
 # out: drwxr-xr-x 1 runner runner   42 Apr 30 01:38 .cache/
 # out: -rw------- 1 runner runner 4322 Apr 30 06:36 cs2520_lec13_exceptions_exercises.md
 # out: -rw------- 1 runner runner 4090 Apr 30 06:44 cs2520_lec13_exercises (2).md
-# out: drwxr-xr-x 1 runner runner   44 Apr 30 10:06 data/
+# out: drwxr-xr-x 1 runner runner   62 Apr 30 10:30 data/
 # out: -rw-r--r-- 1 runner runner 9927 Apr  2  2025 generated-icon.png
-# out: drwxr-xr-x 1 runner runner  172 Apr 30 10:09 .git/
+# out: drwxr-xr-x 1 runner runner  172 Apr 30 10:32 .git/
 # out: -rw-r--r-- 1 runner runner 3077 Feb 27  2024 .gitignore
-# out: -rw-r--r-- 1 runner runner   58 Apr 30 10:12 goog.py
+# out: -rw-r--r-- 1 runner runner  768 Apr 30 10:33 goog.py
 # out: drwxr-xr-x 1 runner runner   44 Apr 30 09:59 Helpers/
 # out: -rw-r--r-- 1 runner runner 9929 Apr 30 09:55 inline_output_v3.py
 # out: drwxr-xr-x 1 runner runner   84 Apr 30 09:54 .local/
