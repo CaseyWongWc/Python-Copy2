aa3df5fb26b5dc77f1f6f3bc031e4710b3d0fbc1
CaseyWongWc <147561210+CaseyWongWc@users.noreply.github.com>
2026-05-01 07:21:34 +0000

    🔄 autosave @ 07:21:34

--- zzzmarathon.py
+++ zzzmarathon.py
@@ -312,3 +312,51 @@ myCar.print_car_speed()
 
 '''
 ##########################
+'''challenge activity
+13.1.1: Derived classes.
+712910.5105864.qx3zqy7
+Jump to level 1
+Type the program's output'''
+
+class Vehicle:
+    def __init__(self):
+        self.speed = 0
+
+    def set_speed(self, speed_to_set):
+        self.speed = speed_to_set
+
+    def print_speed(self):
+        print(self.speed)
+
+
+class Car(Vehicle):
+    def print_car_speed(self):
+        print("Driving at: ", end = "")
+        self.print_speed()
+
+
+class AnimalPowered(Vehicle):
+    def __init__(self):
+        self.animal = ""
+
+    def set_animal(self, animal_to_set):
+        self.animal = animal_to_set
+
+    def print_animal_speed(self):
+        print(f"{self.animal} speed: ", end = "")
+        self.print_speed()
+
+
+myCar = Car()
+chariot = AnimalPowered()
+
+myCar.set_speed(20)
+chariot.set_speed(12)
+chariot.set_animal("Donkey")
+
+myCar.print_car_speed()
+chariot.print_animal_speed()
+
+#Output:
+'''Driving at: 20
+Donkey speed: 12
