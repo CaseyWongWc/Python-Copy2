3297605aa3da25e3a8defb40e9a2147b3c67163c
CaseyWongWc <147561210+CaseyWongWc@users.noreply.github.com>
2026-05-01 07:22:37 +0000

    🔄 autosave @ 07:22:37

--- zzzmarathon.py
+++ zzzmarathon.py
@@ -367,3 +367,43 @@ Donkey speed: 12
 712910.5105864.qx3zqy7
 Jump to level 1
 Type the program's output'''
+
+class Vehicle:
+    def __init__(self):
+        self.speed = 0
+
+    def set_speed(self, speed_to_set):
+        self.speed = speed_to_set
+
+    def print_speed(self):
+        print(self.speed)
+
+
+class Car(Vehicle):
+    def print_car_speed(self):
+        print("Moving at: ", end = "")
+        self.print_speed()
+
+
+class ElectricCar(Car):
+    def __init__(self):
+        self.battery_level = 0
+
+    def set_battery_level(self, level_to_set):
+        self.battery_level = level_to_set
+
+    def print_battery_level(self):
+        print(f"Battery: {self.battery_level}")
+
+
+myCar = ElectricCar()
+myCar.set_speed(60)
+myCar.set_battery_level(5)
+
+myCar.print_car_speed()
+myCar.print_battery_level()
+
+#Output:
+'''Moving at: 60
+Battery: 5
+'''
