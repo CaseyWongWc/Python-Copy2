7abe19391b0903bb7929deae22bb79cba94fbd98
CaseyWongWc <147561210+CaseyWongWc@users.noreply.github.com>
2026-05-01 07:21:13 +0000

    🔄 autosave @ 07:21:13

--- zzzmarathon.py
+++ zzzmarathon.py
@@ -287,6 +287,26 @@ myCar.set_speed(55)
 myCar.print_car_speed()
 #Output:
 '''
+class Vehicle:
+    def __init__(self):
+        self.speed = 0
+
+    def set_speed(self, speed_to_set):
+        self.speed = speed_to_set
+
+    def print_speed(self):
+        print(self.speed)
+
+
+class Car(Vehicle):
+    def print_car_speed(self):
+        print("Moving at: ", end = "")
+        self.print_speed()
+
+
+myCar = Car()
+myCar.set_speed(55)
+myCar.print_car_speed()
 #Output:
 '''Moving at: 55
 
