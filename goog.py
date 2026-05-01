818cb56e599437f4be1a05d705b3c117d9854ac9
CaseyWongWc <147561210+CaseyWongWc@users.noreply.github.com>
2026-05-01 03:53:23 +0000

    AAAAAAAAAAAAAAAAAAAAAAAA

--- goog.py
+++ goog.py
@@ -1,3 +1,5 @@
+
+
 '''challenge activity
 13.1.2: Defining a derived class.
 712910.5105864.qx3zqy7
@@ -10,66 +12,72 @@ Ex: If the input is:
 Tia
 Lynn
 then the output is:
+class DrinkOrder:
+  def __init__(self):
+      self.first_name = 0
+      self.last_name = 0
 
-Milkshake
-First name: Tia, Last name: Lynn
- 
+  def set_first_name(self, first_name_value):
+      self.first_name = first_name_value
 
-1
+  def set_last_name(self, last_name_value):
+      self.last_name = last_name_value
 
-2
-
-3
-
-Check
-
-Next level
-1
-2
-3
-
-Feedback?'''
-class Temp:
-  class DrinkOrder:
-    def __init__(self):
-        self.first_name = 0
-        self.last_name = 0
-
-    def set_first_name(self, first_name_value):
-        self.first_name = first_name_value
-
-    def set_last_name(self, last_name_value):
-        self.last_name = last_name_value
-
-    def display(self):
-        print(f"First name: {self.first_name}, Last name: {self.last_name}")
+  def display(self):
+      print(f"First name: {self.first_name}, Last name: {self.last_name}")
 
 
-    """ Your code goes here """
-    def __init__(self):
-        DrinkOrder.__init__(self)
+""" Your code goes here """
 
-    def print_order(self):
-        print("Milkshake")
+  def __init__(self):
+      DrinkOrder.__init__(self)
+
+  def print_order(self):
+      print("Milkshake")
 
 
-    first_name_value = input()
-    last_name_value = input()
-  
-    customer_order = Milkshake()
-    customer_order.set_first_name(first_name_value)
-    customer_order.set_last_name(last_name_value)
-    customer_order.print_order()
-    customer_order.display()
+first_name_value = input()
+last_name_value = input()
 
-# !err: --- ERROR ---
-# !err: Traceback (most recent call last):
-# !err:   File "<string>", line 46, in <module>
-# !err:   File "goog.py", line 32, in <module>
-# !err:     class Temp:
-# !err:   File "goog.py", line 33, in Temp
-# !err:     class DrinkOrder:
-# !err:   File "goog.py", line 59, in DrinkOrder
-# !err:     customer_order = Milkshake()
-# !err:                      ^^^^^^^^^
-# !err: NameError: name 'Milkshake' is not defined
+customer_order = Milkshake()
+customer_order.set_first_name(first_name_value)
+customer_order.set_last_name(last_name_value)
+customer_order.print_order()
+customer_order.display()
+'''
+
+class DrinkOrder:
+  def __init__(self):
+      self.first_name = 0
+      self.last_name = 0
+
+  def set_first_name(self, first_name_value):
+      self.first_name = first_name_value
+
+  def set_last_name(self, last_name_value):
+      self.last_name = last_name_value
+
+  def display(self):
+      print(f"First name: {self.first_name}, Last name: {self.last_name}")
+
+
+""" Your code goes here """
+class Milkshake(DrinkOrder):
+  def __init__(self):
+    DrinkOrder.__init__(self)
+    self.expiration = ""
+  def __init__(self):
+      DrinkOrder.__init__(self)
+
+  def print_order(self):
+      print("Milkshake")
+
+
+first_name_value = input()
+last_name_value = input()
+
+customer_order = Milkshake()
+customer_order.set_first_name(first_name_value)
+customer_order.set_last_name(last_name_value)
+customer_order.print_order()
+customer_order.display()
