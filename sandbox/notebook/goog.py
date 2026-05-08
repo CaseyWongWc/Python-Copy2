88da21ade1e64b56058a6b6c206acc29c1d268c9
CaseyWongWc <147561210+CaseyWongWc@users.noreply.github.com>
2026-05-08 03:20:11 +0000

    Hi

--- sandbox/notebook/goog.py
+++ sandbox/notebook/goog.py
@@ -16,30 +16,10 @@ with """ '\n _     ___\n#_~`--'__ `===-,\n`.`.     `#.,;《\n,_|_|     ## #《\n
   # f.out: hello
 with bash:
   ls -alf
-  # out: total 16
-  # out: drwxr-xr-x 1 runner runner 866 May  8 03:13 .
-  # out: drwxr-xr-x 1 runner runner 472 May  7 06:13 ..
-  # out: drwxr-xr-x 1 runner runner   0 May  7 06:38 .run_blocks
-  # out: -rw-r--r-- 1 runner runner   0 May  8 02:23 bob2
-  # out: -rw-r--r-- 1 runner runner   2 May  8 02:27 {yes}
-  # out: -rw-r--r-- 1 runner runner   2 May  8 02:31 +------+.      +------+       +------+       +------+      .+------+
-  # out: 
-  # out: drwxr-xr-x 1 runner runner 296 May  8 02:58  '
-  # out:  _     ___
-  # out: #_~`--'__ `===-,
-  # out: `.`.     `#.,
-  # out: drwxr-xr-x 1 runner runner 332 May  8 02:58  '\n _     ___\n#_~`--'__ `===-,\n`.`.     `#.,
-  # out: -rw-r--r-- 1 runner runner   2 May  8 03:10  '
-  # out:  _     ___
-  # out: #_~`--'__ `===-,
-  # out: `.`.     `#.,;《
-  # out: ,_|_|     ## #《
-  # out: `__.__    `####《
-  # out:      ~~< ,###'~
-  # out:         》##'
-  # out: Wolves
-  # out: '
-  # out: -rw-r--r-- 1 runner runner  15 May  8 03:13  '
+  # out: total 4
+  # out: drwxr-xr-x 1 runner runner 256 May  8 03:19 .
+  # out: drwxr-xr-x 1 runner runner 472 May  8 03:19 ..
+  # out: -rw-r--r-- 1 runner runner  15 May  8 03:19  '
   # out:  _     ___
   # out: #_~`--'__ `===-,
   # out: `.`.     `#.,;《
