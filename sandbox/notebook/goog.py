7b67532380e9654807dc46dd48293e8a3aa61fcd
CaseyWongWc <147561210+CaseyWongWc@users.noreply.github.com>
2026-05-13 04:24:32 +0000

    📤 manual push @ 04:24:32

--- sandbox/notebook/goog.py
+++ sandbox/notebook/goog.py
@@ -1533,13 +1533,13 @@ f
 ####################################################participation activity 15.5.2: Array shape.
 with "myfile.py" as RUN:
     #Match the shape to the array.
-    print("(4,)", '["a", "b", "c", "d"]')
-    print("(2, 2, 3)","[ [ [1, 2, 9], [3, 2, 6] ], [ [8, 8, 4], [9, 8, 7] ] ]")
-    print("(1,)","[3.1415]")
-    print("( )", "invalid array")
+    print("(4,)",)
+    print("(2, 2, 3)")
+    print("(1,)")
+    print("( )")
     print("invalid array")
-    print("(2, 4)", "[ [10, 20, 30, 40], [50, 60, 70] ]")
-    print("(4, 2)", "[ [10, 20, 30, 40], [50, 60, 70, 80] ]")
+    print("(2, 4)")
+    print("(4, 2)")
     print('["a", "b", "c", "d"]')
     print("[ [ [1, 2, 9], [3, 2, 6] ], [ [8, 8, 4], [9, 8, 7] ] ]")
     print("[ [1, 2], [3, 4], [5, 6], [7, 8] ]")
