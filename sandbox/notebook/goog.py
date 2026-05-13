35785bf8c792055d4dd0e732f3952f25acf53bf2
CaseyWongWc <147561210+CaseyWongWc@users.noreply.github.com>
2026-05-13 04:57:38 +0000

    📤 manual push @ 04:57:38

--- sandbox/notebook/goog.py
+++ sandbox/notebook/goog.py
@@ -1830,7 +1830,7 @@ with "main1.py" as RUN:
     # out: 
     # out: Finding maximum element in array1
     # out: 40
-with Scratch as f:
+with _ as f:
     import numpy as np
     import time
     hey=[]
@@ -1841,9 +1841,11 @@ with Scratch as f:
     # val: array([[ 100,  400,  900, 1600],
     # val:        [   1,    4,    9,   16]])
 f
+# val: Scratch(np=<module 'numpy' from '/workspaces/Python-Copy2/.venv/lib/python3.12/site-packages/numpy/__init__.py'>, time=<module 'time' (built-in)>, hey=[[10, 20, 30, 40], [1, 2, 3, 4]], hey2=array([[10, 20, 30, 40],
+# val:        [ 1,  2,  3,  4]]), out=[], err=[], outs='')
 INFO()
 # val: /workspaces/Python-Copy2/sandbox/files
-# val: Wed May 13 04:56:56 UTC 2026
+# val: Wed May 13 04:57:15 UTC 2026
 # val: ./myfile.txt
 # val: ./.venv/pyvenv.cfg
 # val: ./Scratch.py
