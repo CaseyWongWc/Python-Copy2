3d0c76a6c1931071f2ff7916246e3e686533fee3
CaseyWongWc <147561210+CaseyWongWc@users.noreply.github.com>
2026-05-13 02:38:48 +0000

    📤 manual push @ 02:38:48

---
+++ sandbox/notebook/goog.py
@@ -0,0 +1,31 @@
+'''15.1 Introduction to data science
+Data science
+Data science is an interdisciplinary field focused on discovering patterns and describing relationships using data. Data science uses techniques from computer science and statistics. Data scientists use computers to write code and store, modify, and visualize large datasets. Data scientists can also build, test, and interpret a data model, a representation of a real-life system that organizes data elements and informs how the elements relate to one another. Ex: The FBI's Most Wanted list is a model that contains data elements such as eye color, hair color, known accomplices, etc. related to each suspect on the list. The data model can be used to make predictions for new data.
+
+participation activity
+15.1.1: Comparing data science, computer science, and statistics.
+
+Start
+Computer scientistsProtect systems from hackersDevelop softwareAnalyze data with algorithmsDesign websitesBuild data storage toolsStatisticiansDesign experimentsDerive new modelsAnalyze data using modelsModify and format datasetsCreate dynamic plots and graphsInterpret resultsData scientists
+Step 1: A circle appears labeled "Computer scientists." Computer science tasks are listed in the circle: design websites, develop software, protect from hackers, analyze data with algorithms, build data storage tools. Step 2: A circle appears labeled "Statisticians." Statistics tasks are listed in the circle: design experiments, derive new models, analyze data using models, and interpret results. Step 3: A circle appears labeled "Data scientists." The circle overlaps with both "Computer scientists" and "Statisticians". Analyze data with algorithms, build data storage tools, analyze data using models, interpret results, are in the data science circle. New tasks appear: modify and format datasets, create dynamic plots and graphs.
+
+Captions
+Computer scientists use programming to design new software and websites, protect computer systems from hackers, implement algorithms, and store data.
+Statisticians design experiments and apply models to discover trends and patterns in a dataset. Statisticians also derive new models using mathematical techniques.
+Data scientists use programming to transform data into meaningful information using graphs, algorithms, and models.
+
+Feedback?
+participation activity
+15.1.2: Data science.
+Many professionals use data, but not all are data scientists. Who is most likely to work on each of the data-related tasks below?
+
+1)
+Creating an interactive graph of product sales in the past 18 months
+Correct
+Data scientists present data in graphs, tables, and interactive visualizations, which help the scientists gather meaningful insights from data.
+2)
+Building a user interface for a data storage system containing data on product sales
+3)
+Designing an experiment to compare three marketing strategies in different markets
+
+Feedback?'''
