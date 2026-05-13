b844009756be12f90a1913e6100736cc362097e2
CaseyWongWc <147561210+CaseyWongWc@users.noreply.github.com>
2026-05-13 03:08:17 +0000

    📤 manual push @ 03:08:17

--- sandbox/notebook/goog.py
+++ sandbox/notebook/goog.py
@@ -310,16 +310,7 @@ with "myfile.txt" as f:
     '''
 #table 15.3.2: Common data science packages in Python.
 with "myfile.py" as f:
-    '''
-    import name	common alias	description
-    numpy	np	numPy includes functions and classes that aid in numerical computation. numPy is used in many other data science packages.
-    pandas	pd	pandas provides methods and classes for tabular and time-series data.
-    sklearn	sk	scikit-learn provides implementations of many machine learning algorithms with a uniform syntax for preprocessing data, specifying models, fitting models with cross-validation, and assessing models.
-    matplotlib.pyplot	plt	Matplotlib allows the creation of data visualizations in Python. The functions mostly expect NumPy arrays.
-    seaborn	sns	seaborn also allows the creation of data visualizations but works better with pandas DataFrame objects.
-    scipy.stats	sp.stats	SciPy provides algorithms and functions for computing problems that arise in science, engineering and statistics. scipy.stats provides the functions for statistics.
-    statsmodels	sm	statsmodels adds functionality to Python to estimate many different kinds of statistical models, make inferences from those models, and explore data.
-    '''
+    
 # val: /workspaces/Python-Copy2/sandbox/files
 # val: Wed May 13 03:04:21 UTC 2026
 # val: ./myfile.txt
