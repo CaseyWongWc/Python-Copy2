257c8a5cf2e0b068fca0a7229a1e3be981582d35
CaseyWongWc <147561210+CaseyWongWc@users.noreply.github.com>
2026-05-13 04:29:32 +0000

    📤 manual push @ 04:29:32

--- sandbox/notebook/goog.py
+++ sandbox/notebook/goog.py
@@ -1572,7 +1572,8 @@ with "myfile.py" as RUN:
 with "myfile.txt":
     '''
     1) How many axes does a 3D array have?
-    2) In a 2D array, which axis is the second or last axis
+    2) In a 2D array, which axis is the second or last axis?
+    
     '''
 INFO()
 # val: /workspaces/Python-Copy2/sandbox/files
