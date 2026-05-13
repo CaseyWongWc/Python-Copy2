9879db336903fc829b95e4022142b9323c3509eb
CaseyWongWc <147561210+CaseyWongWc@users.noreply.github.com>
2026-05-13 00:30:12 +0000

    E

--- sandbox/notebook/goog.py
+++ sandbox/notebook/goog.py
@@ -10,16 +10,16 @@ yes
 # val:         》##'
 # val: Wolves
 # val: '
-with """ '\n _     ___\n#_~`--'__ `===-,\n`.`.     `#.,;《\n,_|_|     ## #《\n`__.__    `####《\n     ~~< ,###'~\n        》##'\nWolves\n'.py""" as f:
+with """ '\n _     ___\n#_~`--'__ `===-,\n`.`.     `#.,;《\n,_|_|     ## #《\n`__.__    `####《\n     ~~< ,###'~\n        》##'\nWolves\n'""" as f:
   print("hello")
   # out: hello
   # f.out: hello
 with bash:
   ls -alf
-  # out: total 4
-  # out: drwxr-xr-x 1 runner runner 256 May  8 03:19 .
+  # out: total 8
+  # out: drwxr-xr-x 1 runner runner 506 May  8 04:11 .
   # out: drwxr-xr-x 1 runner runner 472 May  8 03:19 ..
-  # out: -rw-r--r-- 1 runner runner  15 May  8 03:19  '
+  # out: -rw-r--r-- 1 runner runner  15 May  8 04:11  '
   # out:  _     ___
   # out: #_~`--'__ `===-,
   # out: `.`.     `#.,;《
@@ -29,6 +29,16 @@ with bash:
   # out:         》##'
   # out: Wolves
   # out: '.py
+  # out: -rw-r--r-- 1 runner runner  15 May  8 04:11  '
+  # out:  _     ___
+  # out: #_~`--'__ `===-,
+  # out: `.`.     `#.,;《
+  # out: ,_|_|     ## #《
+  # out: `__.__    `####《
+  # out:      ~~< ,###'~
+  # out:         》##'
+  # out: Wolves
+  # out: '
 h=""" '\n _     ___\n#_~`--'__ `===-,\n`.`.     `#.,//\n,_|_|     ## #《\n`__.__    `####《\n     ~~< ,###'~\n        》##'\nWolves\n'"""
 h
 # val:  '
@@ -41,3 +51,4 @@ h
 # val:         》##'
 # val: Wolves
 # val: '
+
