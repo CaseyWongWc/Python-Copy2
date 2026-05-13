74e0f9a3a64de5e2f65a67f281cd016b473e2428
CaseyWongWc <147561210+CaseyWongWc@users.noreply.github.com>
2026-05-13 03:31:56 +0000

    📤 manual push @ 03:31:56

--- sandbox/files/main.py
+++ sandbox/files/main.py
@@ -1,5 +1,5 @@
 import numpy as np
-# import pandas as pd
+import pandas as pd
 # import sklearn as sk
 # import matplotlib.pyplot as plt
 # import seaborn as sns
--- sandbox/notebook/goog.py
+++ sandbox/notebook/goog.py
@@ -368,11 +368,14 @@ with _:
     with "main.py" as f:
         import numpy as np
         import pandas as pd
+        # !err: ModuleNotFoundError: No module named 'pandas'
+        # !err:   at line 363: #     # import scipy.stats as sp.stats
         # import sklearn as sk
         # import matplotlib.pyplot as plt
         # import seaborn as sns
         # import scipy.stats as sp.stats
         # import statsmodels as sm
     f
-    # val: Scratch(np=<module 'numpy' from '/workspaces/Python-Copy2/.venv/lib/python3.12/site-packages/numpy/__init__.py'>, out=[], err=[], outs='')
+    # val: Scratch(out=[], err=[], outs='')
     
+import pandas as pd
