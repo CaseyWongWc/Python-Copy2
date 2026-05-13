26ee7cc4ec3e3f6705396fc88b0761da8db476a7
CaseyWongWc <147561210+CaseyWongWc@users.noreply.github.com>
2026-05-13 04:00:38 +0000

    📤 manual push @ 04:00:38

--- sandbox/files/main.py
+++ sandbox/files/main.py
@@ -1,7 +1,24 @@
 import numpy as np
-import pandas as pd
-import sklearn as sk
-import matplotlib.pyplot as plt
-import seaborn as sns
-import scipy.stats as sp_stats
-import statsmodels as sm
+
+list1 = [15.5, 25.11, 19.0]
+list2 = [12.2, 1.3, 6.38] 
+
+# Create two 1-dimensional (1D) arrays
+# with the elements of the above lists
+array1 = np.array(list1)
+array2 = np.array(list2)
+
+# Concatenate two lists
+print("Concatenation of list1 and list2 =", end=" ")
+print(list1 + list2)
+print()
+
+# Sum two lists
+print("Sum of list1 and list2 =", end=" ")
+for i in range(len(list1)):
+    print(list1[i] + list2[i], end=" ")  
+print("\n")
+
+# Sum two 1D arrays
+print("Sum of array1 and array2 =", end=" ")
+print(array1 + array2)  
