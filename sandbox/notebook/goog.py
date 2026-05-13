11b2c1468c1cf96aa9b479acca0de183a21402a0
CaseyWongWc <147561210+CaseyWongWc@users.noreply.github.com>
2026-05-13 03:43:44 +0000

    📤 manual push @ 03:43:44

--- sandbox/notebook/goog.py
+++ sandbox/notebook/goog.py
@@ -592,5 +592,6 @@ with _:
         - copy code cell `In[3]`. in the new cell, change the `"weight"` feature to `"horsepower"`.
         - click "file", then "download as". download the Jupyter notebook as a notebook or HTML file.
         '''
-    with "mynotebook"
+    with "myfile.md" as f:
+        
 # INFO()
