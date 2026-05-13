b4bf77b89927428764ef4425c4339d587414c5e4
CaseyWongWc <147561210+CaseyWongWc@users.noreply.github.com>
2026-05-13 04:51:43 +0000

    📤 manual push @ 04:51:43

--- sandbox/notebook/goog.py
+++ sandbox/notebook/goog.py
@@ -1751,7 +1751,7 @@ with "main.py" as f:
     # f.out:  ['e' 'f']
     # f.out:  ['a' 'b']
     # f.out:  ['g' 'h']]
-     my_array = np.array( [  ["c", "d"], ["e", "f"], ["a", "b"], ["g", "h"] ] )
+    my_array = np.array( [  ["c", "d"], ["e", "f"], ["a", "b"], ["g", "h"] ] ) 
 
 
 
