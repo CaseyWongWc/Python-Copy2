ee0fc1e7fd944dbc4695595cec3c65550626f20f
CaseyWongWc <147561210+CaseyWongWc@users.noreply.github.com>
2026-05-13 03:43:23 +0000

    📤 manual push @ 03:43:23

--- sandbox/notebook/goog.py
+++ sandbox/notebook/goog.py
@@ -582,5 +582,14 @@ with "myfile.txt":
     '''
 ####################################################Try 15.4.1: Getting started with Jupyter notebooks.
 with _:
-    
+    with "myfile.txt" as f:
+        '''
+        the Jupyter notebook loads the miles per gallon dataset and creates a scatter plot of miles per gallon against weight and engine size (number of cylinders) for each car. the notebook also calculates summary statistics for weight.
+        
+        - click the double right arrow icon to restart the kernel and run all cells. 
+        - examine the code and text below. 
+        - add a new heading and text inside the first text cell.
+        - copy code cell `In[3]`. in the new cell, change the `"weight"` feature to `"horsepower"`.
+        - click "file", then "download as". download the Jupyter notebook as a notebook or HTML file.
+        '''
 # INFO()
