49fbe339e2ac6b029204b24f635a2440e4166033
CaseyWongWc <147561210+CaseyWongWc@users.noreply.github.com>
2026-05-13 02:49:48 +0000

    📤 manual push @ 02:49:48

--- sandbox/notebook/goog.py
+++ sandbox/notebook/goog.py
@@ -160,7 +160,7 @@ with "myfile.txt":
     variety
     -electronic health records contain data on patient measurements, test results, medical history, image scans, and other characteristics. This variety of data types and formats is a key aspect of big data in healthcare.
 velocity
--wearable devices like a smartwatch can track a patient's exercise, heart rate, and sleeping habits. Data from these devices are sent to a patient's doctor or care team in real time. The speed
+-wearable devices like a smartwatch can track a patient's exercise, heart rate, and sleeping habits. Data from these devices are sent to a patient's doctor or care team in real time. The speed at which this data is generated and transmitted is an example of the velocity aspect of big data in healthcare
     '''
     
 
