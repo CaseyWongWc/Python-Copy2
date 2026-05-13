4e1180214f8a55ec4e4d789c92b4e0e90df8f1c3
CaseyWongWc <147561210+CaseyWongWc@users.noreply.github.com>
2026-05-13 04:54:00 +0000

    📤 manual push @ 04:53:59

--- sandbox/notebook/goog.py
+++ sandbox/notebook/goog.py
@@ -1765,6 +1765,33 @@ with "main.py" as f:
     # f.out:  ['g' 'h']]
 ####################################################participation activity 15.5.6: Math operators and functions.
 with "main1.py" as RUN:
+    import numpy as np
+
+    array1 = np.array([10, 20, 30, 40])
+    array2 = np.array([1, 2, 3, 4])
+
+    # Some common array operations
+
+    print("Adding arrays (array1 + array2)")
+    print(array1 + array2)
+
+    print("\nSubtracting arrays (array1 - array2)")
+    print(array1 - array2)
+
+    print("\nMultiplying arrays (array1 * array2)")
+    print(array1 * array2)
+
+    print("\nCalculating dot product of arrays")
+    print(np.dot(array1, array2))
+
+    print("\nFinding square root of each element in array1")
+    print(np.sqrt(array1))
+
+    print("\nFinding minimum element in array1")
+    print(array1.min())
+
+    print("\nFinding maximum element in array1")
+    print(array1.max())
 
 
 INFO()
