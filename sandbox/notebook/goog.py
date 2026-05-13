da30707439b8bdfd62d4f48b7945a7932290de0b
CaseyWongWc <147561210+CaseyWongWc@users.noreply.github.com>
2026-05-13 03:13:59 +0000

    📤 manual push @ 03:13:59

--- sandbox/notebook/goog.py
+++ sandbox/notebook/goog.py
@@ -323,7 +323,7 @@ with _:
         | statsmodels | sm | statsmodels adds functionality to Python to estimate many different kinds of statistical models, make inferences from those models, and explore data. |
         '''
 
-    with "myfile.py" as f:
+    with "main.py" as f:
             import numpy as np
             # !err: ModuleNotFoundError: No module named 'numpy'
             # !err:   at line 320: | matplotlib.pyplot | plt | Matplotlib allows the creation of data visualizations in Python. The functions mostly expect NumPy arrays. |
