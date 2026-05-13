f48e1c1fc6cc035cf90077c3cf87904cd1bd29ed
CaseyWongWc <147561210+CaseyWongWc@users.noreply.github.com>
2026-05-13 01:31:43 +0000

    📤 manual push @ 01:31:43

--- sandbox/notebook/goog.py
+++ sandbox/notebook/goog.py
@@ -11,7 +11,6 @@ yes
 # val: Wolves
 # val: '
 with """ '\n _     ___\n#_~`--'__ `===-,\n`.`.     `#.,;《\n,_|_|     ## #《\n`__.__    `####《\n     ~~< ,###'~\n        》##'\nWolves\n'""" as f:
-    
   print("hello")
   # out: hello
   # f.out: hello
