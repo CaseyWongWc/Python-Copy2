a77534a6b3488f2c08617219cfb2b5841c5bc544
CaseyWongWc <147561210+CaseyWongWc@users.noreply.github.com>
2026-05-13 05:03:21 +0000

    📤 manual push @ 05:03:21

--- sandbox/notebook/goog.py
+++ sandbox/notebook/goog.py
@@ -1847,7 +1847,7 @@ with _ as f:
     # val: array([], shape=(2, 0), dtype=float64)
     hey2=[[1],[1,2]]
     try:
-        
+        hey2
 f
 # val: Scratch(np=<module 'numpy' from '/workspaces/Python-Copy2/.venv/lib/python3.12/site-packages/numpy/__init__.py'>, time=<module 'time' (built-in)>, hey=[[10, 20, 30, 40], [1, 2, 3, 4]], hey2=array([[10, 20, 30, 40],
 # val:        [ 1,  2,  3,  4]]), hey_1=[[], []], hey_1_2=array([], shape=(2, 0), dtype=float64), out=[], err=[], outs='')
