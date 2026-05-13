649edbf8b49e416b00b269b33f51abc33312e6d1
CaseyWongWc <147561210+CaseyWongWc@users.noreply.github.com>
2026-05-13 04:28:44 +0000

    📤 manual push @ 04:28:44

--- sandbox/notebook/goog.py
+++ sandbox/notebook/goog.py
@@ -1569,6 +1569,7 @@ with "myfile.py" as RUN:
     # out: [ [10, 20, 30, 40], [50, 60, 70, 80] ]
     # out: [3.1415]
 ####################################################participation activity 15.5.4: Array axes.
+with "myfile.txt"
 INFO()
 # val: /workspaces/Python-Copy2/sandbox/files
 # val: Wed May 13 04:21:37 UTC 2026
