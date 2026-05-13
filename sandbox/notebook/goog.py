553aec412f5e5b58c90e5254ee0b4b446c92e8a5
CaseyWongWc <147561210+CaseyWongWc@users.noreply.github.com>
2026-05-13 03:55:44 +0000

    📤 manual push @ 03:55:44

--- sandbox/notebook/goog.py
+++ sandbox/notebook/goog.py
@@ -605,7 +605,7 @@ with _:
     with bash:
         # cat myfile.txt
         #cat First\ Jupyter\ notebook.ipynb | head -n 10
-        cat First\ Jupyter\ notebook.ipynb
+        #cat First\ Jupyter\ notebook.ipynb
         # out: {
         # out:  "cells": [
         # out:   {
