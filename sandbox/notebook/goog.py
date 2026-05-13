efe12afc1907189ff2a69b710b50c340a4978ec8
CaseyWongWc <147561210+CaseyWongWc@users.noreply.github.com>
2026-05-13 03:20:56 +0000

    📤 manual push @ 03:20:56

--- sandbox/notebook/goog.py
+++ sandbox/notebook/goog.py
@@ -338,10 +338,10 @@ with _:
     with "main.py" as f:
         '''
         python3 -m venv .venv
-source .venv/bin/activate
-python -m pip install --upgrade pip
-pip install numpy
-python -c "import numpy as np; print(np.__version__)'''
+        source .venv/bin/activate
+        python -m pip install --upgrade pip
+        pip install numpy
+        python -c "import numpy as np; print(np.__version__)'''
         cmd("ls","-alf")
         cmd()
         # val: myfile.txt
