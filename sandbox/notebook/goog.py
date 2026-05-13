b1592cdd8bae20cc77c086541465b9942066cb6b
CaseyWongWc <147561210+CaseyWongWc@users.noreply.github.com>
2026-05-13 04:23:06 +0000

    📤 manual push @ 04:23:06

--- sandbox/notebook/goog.py
+++ sandbox/notebook/goog.py
@@ -1535,7 +1535,7 @@ with "myfile.py" as RUN:
     #Match the shape to the array.
     print("(4,)", '["a", "b", "c", "d"]')
     print("(2, 2, 3)","[ [ [1, 2, 9], [3, 2, 6] ], [ [8, 8, 4], [9, 8, 7] ] ]")
-    print("(1,)")
+    print("(1,)","")
     print("( )")
     print("invalid array")
     print("(2, 4)")
