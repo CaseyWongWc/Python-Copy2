c71b1c82da1ee5cd653ed66a154d66e7788b716b
CaseyWongWc <147561210+CaseyWongWc@users.noreply.github.com>
2026-05-13 05:00:50 +0000

    📤 manual push @ 05:00:50

--- sandbox/notebook/goog.py
+++ sandbox/notebook/goog.py
@@ -1843,6 +1843,7 @@ with _ as f:
 
     hey=[[],[]]
     hey2=np.array(hey)
+    hey2*hey2
 f
 # val: Scratch(np=<module 'numpy' from '/workspaces/Python-Copy2/.venv/lib/python3.12/site-packages/numpy/__init__.py'>, time=<module 'time' (built-in)>, hey=[[10, 20, 30, 40], [1, 2, 3, 4]], hey2=array([[10, 20, 30, 40],
 # val:        [ 1,  2,  3,  4]]), out=[], err=[], outs='')
