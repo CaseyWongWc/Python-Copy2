0de190c9156bdb85c6ae0ddf0ae62184a2258776
CaseyWongWc <147561210+CaseyWongWc@users.noreply.github.com>
2026-05-13 01:53:08 +0000

    f

--- sandbox/notebook/goog.py
+++ sandbox/notebook/goog.py
@@ -16,9 +16,10 @@ with """ '\n _     ___\n#_~`--'__ `===-,\n`.`.     `#.,;《\n,_|_|     ## #《\n
   # f.out: hello
 with bash:
   ls -alf
-  # out: ..
-  # out: .
-  # out:  '
+  # out: total 4
+  # out: drwxr-xr-x 1 runner runner 250 May 13 01:13 .
+  # out: drwxr-xr-x 1 runner runner 472 May 13 01:13 ..
+  # out: -rw-r--r-- 1 runner runner  15 May 13 01:52  '
   # out:  _     ___
   # out: #_~`--'__ `===-,
   # out: `.`.     `#.,;《
