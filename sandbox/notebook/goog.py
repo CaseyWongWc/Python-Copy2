8555411a665713a38524e8c342523434d2053806
CaseyWongWc <147561210+CaseyWongWc@users.noreply.github.com>
2026-05-13 02:54:40 +0000

    📤 manual push @ 02:54:40

--- sandbox/notebook/goog.py
+++ sandbox/notebook/goog.py
@@ -168,20 +168,7 @@ with "myfile.txt":
 ##############################################################################
 #skipped
 ##############################################################################
-'''Common data science packages
-Many Python packages exist for data manipulation, visualization, and modeling. Different packages provide different functionality, so a data science project will use multiple packages. Ex: Imagine a scientist wants to classify hawk populations in Iowa from a large dataset. First the data is loaded into special data structures using pandas, then classification algorithms are run from scikit-learn, and then the results are visualized using seaborn. The unique capabilities of each package provide a data scientist with a comprehensive set of tools for almost any task.
 
-Table 15.3.2: Common data science packages in Python.
-Import name	Common alias	Description
-numpy	np	NumPy includes functions and classes that aid in numerical computation. NumPy is used in many other data science packages.
-pandas	pd	pandas provides methods and classes for tabular and time-series data.
-sklearn	sk	scikit-learn provides implementations of many machine learning algorithms with a uniform syntax for preprocessing data, specifying models, fitting models with cross-validation, and assessing models.
-matplotlib.pyplot	plt	Matplotlib allows the creation of data visualizations in Python. The functions mostly expect NumPy arrays.
-seaborn	sns	seaborn also allows the creation of data visualizations but works better with pandas DataFrame objects.
-scipy.stats	sp.stats	SciPy provides algorithms and functions for computing problems that arise in science, engineering and statistics. scipy.stats provides the functions for statistics.
-statsmodels	sm	statsmodels adds functionality to Python to estimate many different kinds of statistical models, make inferences from those models, and explore data.
-
-'''
 INFO()
 # val: /workspaces/Python-Copy2/sandbox/files
 # val: Wed May 13 02:45:55 UTC 2026
