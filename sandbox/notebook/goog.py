864fe7612f43eb36eba6a7206d4471fab05cf294
CaseyWongWc <147561210+CaseyWongWc@users.noreply.github.com>
2026-05-13 03:13:26 +0000

    📤 manual push @ 03:13:26

--- sandbox/notebook/goog.py
+++ sandbox/notebook/goog.py
@@ -326,12 +326,12 @@ with _:
     with "myfile.py" as f:
     # !err: SyntaxError: invalid syntax (body line 6)
             import numpy as np
-            import pandas as pd
-            import sklearn as sk
-            import matplotlib.pyplot as plt
-            import seaborn as sns
-            import scipy.stats as sp.stats
-            import statsmodels as sm
+            # import pandas as pd
+            # import sklearn as sk
+            # import matplotlib.pyplot as plt
+            # import seaborn as sns
+            # import scipy.stats as sp.stats
+            # import statsmodels as sm
     f
     # val: Scratch(out=[], err=[], outs='')
 
