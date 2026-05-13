40d7e4359985f1770e55a66018747737451ae04d
CaseyWongWc <147561210+CaseyWongWc@users.noreply.github.com>
2026-05-13 03:59:52 +0000

    📤 manual push @ 03:59:52

--- sandbox/notebook/goog.py
+++ sandbox/notebook/goog.py
@@ -1455,7 +1455,30 @@ Provide section feedback
 '''
 ####################################################Figure 15.5.1: Summing arrays vs summing lists.
 with "main.py" as f:
-    
+import numpy as np
+
+list1 = [15.5, 25.11, 19.0]
+list2 = [12.2, 1.3, 6.38] 
+
+# Create two 1-dimensional (1D) arrays
+# with the elements of the above lists
+array1 = np.array(list1)
+array2 = np.array(list2)
+
+# Concatenate two lists
+print("Concatenation of list1 and list2 =", end=" ")
+print(list1 + list2)
+print()
+
+# Sum two lists
+print("Sum of list1 and list2 =", end=" ")
+for i in range(len(list1)):
+    print(list1[i] + list2[i], end=" ")  
+print("\n")
+
+# Sum two 1D arrays
+print("Sum of array1 and array2 =", end=" ")
+print(array1 + array2)  
 
 INFO()
 # val: /workspaces/Python-Copy2/sandbox/files
