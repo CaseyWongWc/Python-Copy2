e0138f0f4c03514eb4caadfc43028d5ff1886b06
CaseyWongWc <147561210+CaseyWongWc@users.noreply.github.com>
2026-05-13 03:04:10 +0000

    📤 manual push @ 03:04:10

--- sandbox/notebook/goog.py
+++ sandbox/notebook/goog.py
@@ -310,7 +310,7 @@ with '''PA 15.3.1: Python for data science.txt''' as f:
 with "scratch.py" as f:
     
 with bash:
-    
+    rm
 INFO()
 # val: /workspaces/Python-Copy2/sandbox/files
 # val: Wed May 13 03:03:51 UTC 2026
