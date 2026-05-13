53544b66a2fcd194be627f660c9c435e96faeae2
CaseyWongWc <147561210+CaseyWongWc@users.noreply.github.com>
2026-05-13 04:15:32 +0000

    📤 manual push @ 04:15:31

--- sandbox/notebook/goog.py
+++ sandbox/notebook/goog.py
@@ -1517,6 +1517,7 @@ with Scratch as f:
     hey=[]
     hey+=[1,2,3]
     hey+=[4,5,6]
+    
     hey2=np.array(hey)
 f
 # val: Scratch(np=<module 'numpy' from '/workspaces/Python-Copy2/.venv/lib/python3.12/site-packages/numpy/__init__.py'>, time=<module 'time' (built-in)>, out=[], err=[], outs='')
