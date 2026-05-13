d9d2c583f458549ba8fe20beb6cf346a5d033f34
CaseyWongWc <147561210+CaseyWongWc@users.noreply.github.com>
2026-05-13 02:45:45 +0000

    📤 manual push @ 02:45:45

--- sandbox/notebook/goog.py
+++ sandbox/notebook/goog.py
@@ -71,7 +71,7 @@ with Scratch:
         f.write("Adelie,Biscoe,36.3,19.3,180,3250,male\n")
         f.write("Gentoo,Biscoe,45.3,14.5,210,4500,male\n")
         f.write("Dream,Biscoe,18.1,8.5,150,1500,male\n")
-
+# 
 
 
 
